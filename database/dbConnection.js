95aa113f6c66f84b288e4d91fa7e98c4c70617c7
shekmohamed97 <shek37576@gamail.com>
2024-08-01 11:09:05 +0530

    dbConnection updae 3

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection=()=>{
-    mongoose.connect(process.env.MONGO_URL,{    dbName:"",
+    mongoose.connect(process.env.MONGODB_URL,{    dbName:"",
         // useNewUrlParser:true,
         // useUnifiedTopology:true,
         // serverSelectionTimeoutMS:10000,
